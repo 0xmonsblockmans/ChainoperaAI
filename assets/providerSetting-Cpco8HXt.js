d0db6977702677bcd9a49ddda6f53446fff46ca6
0xmonsblockmans <terryvenusdev520@gmail.com>
2025-01-29 11:20:52 -0800

    update

--- assets/providerSetting-Cpco8HXt.js
+++ assets/providerSetting-Cpco8HXt.js
@@ -4,9 +4,9 @@ const a = {
         Linkedin: "https://www.linkedin.com/company/chainopera",
         Medium: "https://medium.com/@chainopera",
         Docs: "https://docs.chainopera.ai/",
-        Telegram: "https://x.com/ChainOperaAIChainOperaAI",
+        Telegram: "https://x.com/",
         Career: "https://jobs.ashbyhq.com/Chainopera",
-        TelegramMain: "https://x.com/ChainOperaAIChainOperaAI"
+        TelegramMain: "https://x.com/"
     },
     e = {
         "fedml-official": "7fdd16f8e2f9438e97d71c1cc62898ed",
